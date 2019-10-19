0fc03a29949c7744331df61577226a297db488a5
Hayden Housen <hayden@haydenhousen.com>
2019-10-18 23:04:00 -0400

    Create basic ai model and write video frame auto sorting script that uses the basic model

---
+++ Dataset/scraper-scripts/4-auto_sort.py
@@ -0,0 +1,38 @@
+import os, operator
+from fastai.vision import *
+
+videos_dir = '../videos'
+
+models_path = "../../Models/slide-classifier/saved-models/"
+learn = load_learner(models_path)
+
+def model_predict(img_path):
+    img = open_image(img_path)
+    pred_class,pred_idx,outputs = learn.predict(img)
+    model_results = outputs.numpy().tolist()
+    model_results_percent = [i * 100 for i in model_results]
+    classes = learn.data.classes
+    final = dict(zip(classes, model_results_percent))
+    return final
+
+for item in os.listdir(videos_dir):
+    current_dir = os.path.join(videos_dir, item)
+    frames_dir = current_dir + "/frames"
+    frames_sorted_dir = current_dir + "/frames_sorted"
+    if os.path.isdir(current_dir) and os.path.exists(frames_dir):
+        print("Video Folder " + item + " with Frames Directory Found!")
+        frames = os.listdir(frames_dir)
+        num_frames = len(frames)
+        for idx, frame in enumerate(frames):
+            print("Progress: " + str(idx) + "/" + str(num_frames))
+            current_frame_path = os.path.join(frames_dir, frame)
+            # run classification
+            predictions = model_predict(current_frame_path)
+            # get key with maximum value from the returned `predictions` dictionary
+            best_guess = max(predictions.items(), key=operator.itemgetter(1))[0]
+            print("AI Predicts: " + best_guess)
+
+            classified_image_dir = frames_sorted_dir + '/' + best_guess
+            if not os.path.exists(classified_image_dir):
+                os.makedirs(classified_image_dir)
+            os.system('mv ' + current_frame_path + ' ' + classified_image_dir)
