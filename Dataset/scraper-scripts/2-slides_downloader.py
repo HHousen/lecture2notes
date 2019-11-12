96d819e0ac057c8ec0c6177dfdf1f8b4cf93fcfc
Hayden Housen <hayden@haydenhousen.com>
2019-11-11 20:04:27 -0500

    Set downloaded status in CSV datasets to False and fix slides_downloader scraper script

--- Dataset/scraper-scripts/2-slides_downloader.py
+++ Dataset/scraper-scripts/2-slides_downloader.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 from pathlib import Path
+from tqdm import tqdm
 
 method = sys.argv[1]
 csv_path = Path("../slides-dataset.csv")
@@ -10,10 +11,10 @@ output_dir = Path("../slides/pdfs")
 if method == "csv":
     # python slides_downloader.py csv
     df = pd.read_csv(csv_path, index_col=0)
-    for index, row in df.iterrows():
+    for index, row in tqdm(df.iterrows(), total=len(df.index), desc="Downloading Slideshows"):
         if row['downloaded'] == False:
             link = row['pdf_link']
-            os.system('wget ' + link + ' -P ' + output_dir)
+            os.popen('wget -q ' + link + ' -P ' + str(output_dir))
             row['downloaded'] = True # NOT WORKING
 else:
     # python slides_downloader.py https://ocw.mit.edu/courses/history/21h-343j-making-books-the-renaissance-and-today-spring-2016/lecture-slides/MIT21H_343JS16_Print.pdf
