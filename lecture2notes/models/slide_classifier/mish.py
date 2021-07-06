572ed0cf64d64b5b43332c44ac83e535861580b7
Hayden Housen <hayden@haydenhousen.com>
2021-07-06 19:04:17 -0400

    Fix mish docstrings

--- lecture2notes/models/slide_classifier/mish.py
+++ lecture2notes/models/slide_classifier/mish.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 def f_mish(input, inplace=False):
     """
     Applies the mish function element-wise:
-    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    :math:`mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))`
     """
     return input * torch.tanh(F.softplus(input))
 
@@ -16,12 +16,12 @@ def f_mish(input, inplace=False):
 class mish(nn.Module):
     """
     Applies the mish function element-wise:
-    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    :math:`mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))`
 
     Shape:
-        - Input: (N, *) where * means, any number of additional
+        - Input: ``(N, *)`` where ``*`` means, any number of additional
           dimensions
-        - Output: (N, *), same shape as the input
+        - Output: ``(N, *)``, same shape as the input
 
     Examples:
         >>> m = mish()
